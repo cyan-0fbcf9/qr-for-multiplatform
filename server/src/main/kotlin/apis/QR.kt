f0189d161d9166c0b644925430b7c25d9b275916
cyan <namid224y@gmail.com>
2020-12-31 16:42:33 +0900

    update api process

--- server/src/main/kotlin/apis/QR.kt
+++ server/src/main/kotlin/apis/QR.kt
@@ -1,16 +1,33 @@
 package apis
 
+import extenstion.duplicate
+import extenstion.toByteArray
+import logics.graphics.RenderingLogic
+import logics.graphics.TransformationLogic
 import qr.QRGenerator
-import java.io.ByteArrayOutputStream
-import javax.imageio.ImageIO
+import services.HttpService
+import java.awt.image.BufferedImage
 
 object QR {
     private val qrGenerator: QRGenerator = QRGenerator()
+    private val renderingLogic: RenderingLogic = RenderingLogic()
+    private val transformationLogic: TransformationLogic = TransformationLogic()
 
     fun generate(value: String): ByteArray {
         val qrImage = qrGenerator.generate(value, 512)
-        return ByteArrayOutputStream().apply {
-            ImageIO.write(qrImage, "png", this)
-        }.toByteArray()
+        return qrImage.toByteArray("png")
+    }
+
+    suspend fun generate(value: String, stackedImageUrl: String): ByteArray {
+        val imageType = BufferedImage.TYPE_4BYTE_ABGR
+        val stackedImage =
+            HttpService.downloadImage(stackedImageUrl)?.duplicate(specificationType = imageType)
+                ?: throw NullPointerException("returned null")
+        val qrImage = qrGenerator.generate(value, 512).duplicate(specificationType = imageType)
+        renderingLogic.stackImageOnCenter(
+            baseImage = qrImage,
+            stackedImage = transformationLogic.resize(stackedImage, 50)
+        )
+        return qrImage.toByteArray("png")
     }
 }
