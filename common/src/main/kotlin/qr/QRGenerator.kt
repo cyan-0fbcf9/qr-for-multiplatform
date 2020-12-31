c91bf8f359c569ae07d44fc37bef976aa938b958
cyan <namid224y@gmail.com>
2020-12-31 21:21:35 +0900

    fix margin and level of qr code

--- common/src/main/kotlin/qr/QRGenerator.kt
+++ common/src/main/kotlin/qr/QRGenerator.kt
@@ -1,8 +1,10 @@
 package qr
 
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import java.awt.image.BufferedImage
 
 class QRGenerator {
@@ -11,7 +13,17 @@ class QRGenerator {
     }
 
     fun generate(value: String, size: Int): BufferedImage {
-        val bitmap = writer.encode(value, BarcodeFormat.QR_CODE, size, size)
+        val bitmap = writer.encode(
+            value,
+            BarcodeFormat.QR_CODE,
+            size,
+            size,
+            mapOf(
+                EncodeHintType.MARGIN to 1,
+                EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.Q,
+                EncodeHintType.QR_VERSION to 6
+            )
+        )
         return MatrixToImageWriter.toBufferedImage(bitmap)
     }
 }
