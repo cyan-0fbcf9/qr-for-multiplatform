4459a6edf9c5ec86bdb29d420b1924cf76385628
cyan <namid224y@gmail.com>
2021-03-16 12:34:18 +0900

    fix

---
+++ nativeCommon/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("multiplatform") version "1.4.20"
+}
+
+group = "cyan0fbcf9"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    macosX64()
+    jvm()
+
+    sourceSets {
+
+    }
+}
