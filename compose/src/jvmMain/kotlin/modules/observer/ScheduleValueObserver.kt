f32e16ffd6ebcb5944d7b31f29dd2b9806cd40df
Cyan-on-win <namid224y@gmail.com>
2021-04-13 09:15:15 +0900

    fix code structure

--- compose/src/jvmMain/kotlin/modules/observer/ScheduleValueObserver.kt
+++ compose/src/jvmMain/kotlin/modules/observer/ScheduleValueObserver.kt
@@ -5,5 +5,4 @@ import java.util.*
 
 interface ScheduleValueObserver<T> : ValueObserver<T>, ScheduleController {
     fun scheduleObserveTask(task: TimerTask): Unit
-    fun tryUpdating(newValue: T): Boolean
 }
