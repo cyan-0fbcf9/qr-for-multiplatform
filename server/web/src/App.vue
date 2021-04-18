81618d37032c571f901b5c85d68940c0416f031c
Cyan-on-win <namid224y@gmail.com>
2021-04-18 11:03:11 +0900

    vue head

--- server/web/src/App.vue
+++ server/web/src/App.vue
@@ -1,32 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Home</router-link>-->
+    <!--      <router-link to="/about">About</router-link>-->
+    <!--      <router-link to="/download">Download</router-link>-->
+    <!--    </div>-->
     <router-view />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      }
+    ]
+  }
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP:300&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans JP', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $font-color;
 }
 
 #nav {
-  padding: 30px;
+  background-color: $gray-color;
 }
 
 #nav a {
+  color: $font-color;
+  display: inline-block;
+  font-size: 1.2em;
   font-weight: bold;
-  color: #2c3e50;
+  padding: 12px 18px;
+  text-decoration: none;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: $background-color;
+  }
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: $background-color;
 }
 </style>
---
+++ server/web/src/pages/Download.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="download-page">
+    <div class="title">
+      ダウンロード
+    </div>
+    <div class="download-content-wrapper">
+      <div class="download-content">
+        <div class="logo">
+          <img src="@/assets/logo.png" alt="logo" width="256" />
+        </div>
+        <div class="download-content-title">
+          QR Screenshot
+        </div>
+        <div class="download-content-description">
+          ディスプレイに表示されているQRコードを読み取れるQRスキャナ
+        </div>
+        <div class="download-link-wrapper">
+          <div class="link-for-macos download-link">
+            <div class="platform-target">macOS</div>
+            <a href="#" target="_blank">
+              <span class="material-icons">file_download</span>
+              <span class="package-name">dmg</span>
+            </a>
+          </div>
+          <div class="link-for-windows download-link">
+            <div class="platform-target">Windows</div>
+            <a href="#" target="_blank">
+              <span class="material-icons">file_download</span>
+              <span class="package-name">msi</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Download'
+};
+</script>
+
+<style scoped lang="scss">
+.download-page {
+  background-color: $background-color;
+  font-size: 1.2rem;
+  height: 100vh;
+  width: 100vw;
+}
+
+.title {
+  color: $font-color;
+  font-size: 3.5em;
+  padding: 16px 0;
+}
+
+.download-content {
+  .download-content-title {
+    color: $primary-color;
+    font-size: 3em;
+    font-weight: bold;
+  }
+  .download-content-description {
+    margin: 8px 0;
+  }
+  .download-link-wrapper {
+    display: grid;
+    justify-items: center;
+    margin: 16px 0;
+
+    .download-link {
+      align-items: center;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      margin: 16px 0;
+
+      .platform-target {
+        margin: 0 12px;
+      }
+
+      a {
+        align-items: center;
+        border-radius: 8px;
+        color: white;
+        background-color: $primary-color;
+        display: flex;
+        justify-content: center;
+        padding: 8px;
+        text-decoration: none;
+        width: 150px;
+
+        .package-name {
+          padding: 0 8px;
+        }
+      }
+    }
+  }
+}
+</style>
