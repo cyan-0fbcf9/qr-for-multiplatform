e766be4d837eeb36130df73835b3b957d762ef6a
Cyan-on-win <namid224y@gmail.com>
2021-04-19 09:31:44 +0900

    fix test

--- server/src/test/kotlin/apis/QRApiTest.kt
+++ server/src/test/kotlin/apis/QRApiTest.kt
@@ -19,7 +19,7 @@ class QRApiTest {
     fun testGenerateQrWithStackedImage(): Unit = runBlocking {
         val generatedImage = QR.generate(
             value = "https://google.com",
-            stackedImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/1200px-Kotlin-logo.svg.png"
+            stackedImageUrl = "https://firebasestorage.googleapis.com/v0/b/free-experimental.appspot.com/o/public%2Fcyan-icon-v2.png?alt=media&token=d7979d25-f179-48df-907e-cc3edcb42864"
         )
         Truth.assertThat(generatedImage).apply {
             isInstanceOf(ByteArray::class.java)
